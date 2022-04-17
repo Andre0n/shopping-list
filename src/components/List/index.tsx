05a1150e1397713c3d420a8a189c43e7ca994e0e
Andre0n <andreon098@gmail.com>
2022-04-17 18:14:56 -0300

    refactory: migrate responsabilty of List component Move the add and show modal functions to App component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,33 +1,15 @@
-import GlobalStyle from "../styles/global.module.css";
+import GlobalStyle from "../../styles/global.module.css";
 import ListStyle from "./List.module.css";
 
-import { FaPlus } from "react-icons/fa";
+import { LItem } from "../../types/LItem";
 import { ListItem } from "../ListItem";
-import { ActionButton } from "../ActionButton";
-import { useState } from "react";
-import { Moddal } from "../Moddal";
 
-interface LItem {
-    title: string;
-    obs?: string;
+interface Props {
+    items: LItem[];
+    removeItem: Function;
 }
 
-export const List: React.FC = (): JSX.Element => {
-    const [items, setItems] = useState<LItem[]>([]);
-    const [shwModal, setShowModal] = useState<boolean>(false);
-
-    const addItem = (title: string, obs: string): void => {
-        const item: LItem = { title, obs };
-        items.push(item);
-        setItems([...items]);
-        setShowModal(false);
-    };
-
-    const removeItem = (index: number): void => {
-        items.splice(index, 1);
-        setItems([...items]);
-    };
-
+export const List = ({ items, removeItem }: Props): JSX.Element => {
     return (
         <ul className={`${GlobalStyle.flex_column} ${ListStyle.list}`}>
             {items.map((item: LItem, index: number) => {
@@ -40,19 +22,6 @@ export const List: React.FC = (): JSX.Element => {
                     />
                 );
             })}
-
-            <Moddal show={shwModal} addItem={addItem} />
-
-            <ActionButton
-                Icon={FaPlus}
-                action={() => {
-                    if (!shwModal) setShowModal(!shwModal);
-                }}
-                customClasses={`${GlobalStyle.rounded_full} 
-                                ${GlobalStyle.bg_succes} 
-                                ${GlobalStyle.white} 
-                                ${ListStyle.button_add}`}
-            />
         </ul>
     );
 };
