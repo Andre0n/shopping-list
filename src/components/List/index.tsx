02204f42e958f16b145173f1acd824ea263818b1
Andre0n <andreon098@gmail.com>
2022-04-17 13:40:40 -0300

    Add Modal component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -5,16 +5,28 @@ import { FaPlus } from "react-icons/fa";
 import { ListItem } from "../ListItem";
 import { ActionButton } from "../ActionButton";
 import { useState } from "react";
+import { Moddal } from "../Moddal";
 
 interface LItem {
     title: string;
     obs?: string;
 }
 
-export const List = (): JSX.Element => {
-    const [items, setItems] = useState([]);
+export const List: React.FC = (): JSX.Element => {
+    const [items, setItems] = useState<LItem[]>(
+        new Array<LItem>(10).fill({
+            title: "Açucar",
+            obs: "dakdskdçskdaçsdkakdkasdkasçdkaçdkaçsd",
+        })
+    );
+    const [shwModal, setShowModal] = useState<boolean>(false);
 
-    const addItem = (): void => {};
+    const addItem = (title: string, obs: string): void => {
+        const item: LItem = { title, obs };
+        items.push(item);
+        setItems([...items]);
+        setShowModal(false);
+    };
 
     const removeItem = (index: number): void => {
         items.splice(index, 1);
@@ -33,9 +45,14 @@ export const List = (): JSX.Element => {
                     />
                 );
             })}
+
+            <Moddal show={shwModal} addItem={addItem} />
+
             <ActionButton
                 Icon={FaPlus}
-                action={addItem}
+                action={() => {
+                    if (!shwModal) setShowModal(!shwModal);
+                }}
                 customClasses={`${GlobalStyle.rounded_full} 
                                 ${GlobalStyle.bg_succes} 
                                 ${GlobalStyle.white} 
